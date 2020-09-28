5272e48a7bad1ec4a1ad71788897dee52640b5cc
Vigo Vlugt <vigovlugt@gmail.com>
2020-09-28 15:12:29 +0200

    Add netlify config

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import Head from "next/head";
+import { useEffect } from "react";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  useEffect(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", (user) => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      Welcome to Next.js!
+    </div>
+  );
 }
 
 export default HomePage;
